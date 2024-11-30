3dde488ce1de59dac38ccf0d30d421895aac4d39
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-11-30 20:42:01 +0000

    add first test and class

--- theatre-booking-2-solution/src/employees/employees.test.ts
+++ theatre-booking-2-solution/src/employees/employees.test.ts
@@ -1,10 +1,12 @@
-import { beforeEach, describe, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { EmployeeManagementSystem } from './employees';
 
 describe('Employee Management System', () => {
-  let EMS;
+  let EMS: EmployeeManagementSystem;
   beforeEach(() => {
     EMS = new EmployeeManagementSystem();
   });
-  it('should not allow non-existing employee to be found', () => {});
+  it('should not allow non-existing employee to be found', () => {
+    expect(EMS.getEmployeebyId('dev99')).toBeUndefined();
+  });
 });
